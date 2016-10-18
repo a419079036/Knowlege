f4aad5687cc93082dda2d52d9cd8cd82b12e4d73
liuliang <419079036@qq.com>
2016-10-18 19:35:05 +0800

    第二次

--- app/src/main/java/com/example/administrator/knowlege/ErWeiMaActivity.java
+++ app/src/main/java/com/example/administrator/knowlege/ErWeiMaActivity.java
@@ -1,11 +1,10 @@
 package com.example.administrator.knowlege;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,14 +15,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class ErWeiMaActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+public class ErWeiMaActivity extends AppCompatActivity implements SurfaceHolder.Callback
+{
     private static final String TAG = ErWeiMaActivity.class.getSimpleName();
     @BindView(R.id.btn_begin)
     Button mButton;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_er_wei_ma);
         ButterKnife.bind(this);
@@ -32,37 +33,45 @@ public class ErWeiMaActivity extends AppCompatActivity implements SurfaceHolder.
 //            surfaceView.getHolder().addCallback(this);
 //        }
     }
+
     @OnClick(R.id.btn_begin)
-    public void beginOnclick(View view){
+    public void beginOnclick(View view)
+    {
         Intent intent = new Intent(Intents.Scan.ACTION);
-        startActivityForResult(intent,1000);
+        startActivityForResult(intent, 1000);
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==1000) {
-            if (data!=null) {
+        if (requestCode == 1000)
+        {
+            if (data != null)
+            {
                 String result = data.getStringExtra("result");
-                Log.d(TAG, "onActivityResult: "+result);
-                Toast.makeText(this,"result"+result,Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onActivityResult: " + result);
+                Toast.makeText(this, "result" + result, Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    public void surfaceCreated(SurfaceHolder holder)
+    {
 
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
 
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
 
     }
 }
